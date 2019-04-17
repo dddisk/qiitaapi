7aad45dcb29de361941dfa313f701a109cc407b2
Daisuke Shiroma <13da144@gmail.com>
2019-04-17 21:01:03 +0900

    appear qiita api

---
+++ qiitaapi/QiitaCell.swift
@@ -0,0 +1,9 @@
+//
+//  QiitaCell.swift
+//  qiitaapi
+//
+//  Created by shiroma_daisuke on 2019/04/17.
+//  Copyright © 2019 shiroma_daisuke. All rights reserved.
+//
+
+import Foundation
--- qiitaapi/QiitaViewController.swift
+++ qiitaapi/QiitaViewController.swift
@@ -1,7 +1,55 @@
 import UIKit
 
-class ViewController: UIViewController {
+// qiitaのapiリストhttps://qiita.com/api/v2/docs
+struct QiitaStruct: Codable {
+    var title: String
+    var user: User
+    struct User: Codable {
+        var name: String
+//        var eventUrl: String
+//        var startedAt: String
+//        private enum CodingKeys: String, CodingKey {
+//            case title
+//            case eventUrl = "event_url"
+//            case startedAt = "started_at"
+//        }
+    }
+}
+
+
+class QiitaViewModel {
+    static func fetchArticle(completion: @escaping ([QiitaStruct]) -> Swift.Void) {
+
+        let url = "https://qiita.com/api/v2/items"
+
+        guard var urlComponents = URLComponents(string: url) else {
+            return
+        }
+
+        urlComponents.queryItems = [
+            URLQueryItem(name: "per_page", value: "50"),
+        ]
+
+        let task = URLSession.shared.dataTask(with: urlComponents.url!) { data, response, error in
+
+            guard let jsonData = data else {
+                return
+            }
+
+            do {
+                let articles = try JSONDecoder().decode([QiitaStruct].self, from: jsonData)
+                completion(articles)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
+    }
+}
+class QiitaViewController: UIViewController {
+    let name:String = "s"
     private var tableView = UITableView()
+    fileprivate var articles: [QiitaStruct] = []
 
     let TODO = ["牛乳を買う", "掃除をする", "アプリ開発の勉強をする"]
 
@@ -10,24 +58,33 @@ class ViewController: UIViewController {
         tableView.dataSource = self
         tableView.frame = view.frame
         view.addSubview(tableView)
+        QiitaViewModel.fetchArticle(completion: { (articles) in
+            self.articles = articles
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        })
     }
 }
 
-extension ViewController: UITableViewDataSource {
+extension QiitaViewController: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // セルを取得する
+
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
-        // セルに表示する値を設定する
-        cell.textLabel!.text = TODO[indexPath.row]
+        let article = articles[indexPath.row]
+        cell.textLabel?.text = article.title
+        cell.detailTextLabel?.text = article.user.name
         return cell
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return TODO.count
+        return articles.count
     }
 
 }
+
+
 //class ViewController: UIViewController {
 //    var y = 0
 //    var number = 0
