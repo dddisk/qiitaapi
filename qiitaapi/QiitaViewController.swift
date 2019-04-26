afaacdbcd5fcb2bff2e5fc589d8e4f8e3dc2e973
dddisk <13da144@gmail.com>
2019-04-26 15:12:25 +0900

    test

--- qiitaapi/QiitaViewController.swift
+++ qiitaapi/QiitaViewController.swift
@@ -6,13 +6,6 @@ struct QiitaStruct: Codable {
     var user: User
     struct User: Codable {
         var name: String
-//        var eventUrl: String
-//        var startedAt: String
-//        private enum CodingKeys: String, CodingKey {
-//            case title
-//            case eventUrl = "event_url"
-//            case startedAt = "started_at"
-//        }
     }
 }
 
