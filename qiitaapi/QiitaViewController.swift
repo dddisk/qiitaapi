39beaa4ba725f84fb8a074f381ca0c0f6297aed0
Daisuke Shiroma <13da144@gmail.com>
2019-05-08 18:18:50 +0900

    Add customcell class

--- qiitaapi/QiitaViewController.swift
+++ qiitaapi/QiitaViewController.swift
@@ -12,6 +12,86 @@ struct QiitaStruct: Codable {
 }
 
 
+class CustomTableViewCell: UITableViewCell
+{
+    var contentText:String = "";
+    var contentLabel = UILabel();
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String!)
+    {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.contentLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentLabel.backgroundColor=UIColor.cyan
+        contentLabel.font = UIFont.systemFont(ofSize: 22)
+        contentLabel.sizeToFit()
+        contentLabel.numberOfLines = 0
+        self.addSubview(contentLabel);
+        self.ajustAutoLayout()
+    }
+
+    func ajustAutoLayout()  {
+
+        let top = NSLayoutConstraint(item: self.contentLabel,
+                                     attribute: NSLayoutConstraint.Attribute.top,
+                                     relatedBy: NSLayoutConstraint.Relation.equal,
+                                     toItem: self,
+                                     attribute: NSLayoutConstraint.Attribute.top,
+                                     multiplier: 1.0,
+                                     constant: 30)
+
+        let bottom = NSLayoutConstraint(item: self.contentLabel,
+                                        attribute: NSLayoutConstraint.Attribute.bottom,
+                                        relatedBy: NSLayoutConstraint.Relation.equal,
+                                        toItem: self,
+                                        attribute: NSLayoutConstraint.Attribute.bottom,
+                                        multiplier: 1.0,
+                                        constant: -30)
+
+        let left = NSLayoutConstraint(item: self.contentLabel,
+                                      attribute: NSLayoutConstraint.Attribute.leading,
+                                      relatedBy: NSLayoutConstraint.Relation.equal,
+                                      toItem: self,
+                                      attribute: NSLayoutConstraint.Attribute.leading,
+                                      multiplier: 1.0,
+                                      constant: 50)
+
+        let right = NSLayoutConstraint(item: self.contentLabel,
+                                       attribute: NSLayoutConstraint.Attribute.trailing,
+                                       relatedBy: NSLayoutConstraint.Relation.equal,
+                                       toItem: self,
+                                       attribute: NSLayoutConstraint.Attribute.trailing,
+                                       multiplier: 1.0,
+                                       constant: -50)
+        self.addConstraint(top)
+        self.addConstraint(bottom)
+        self.addConstraint(left)
+        self.addConstraint(right)
+    }
+
+    func setContentLabelText(text:String){
+        let textFontStyle: String = "Helvetica"
+        let textFontSize: CGFloat = 20.5
+        let textFont = UIFont(name: textFontStyle, size: textFontSize)!
+        let textStyle = NSMutableParagraphStyle()
+        textStyle.lineSpacing = 18.0
+        textStyle.paragraphSpacing = 18.0
+        textStyle.alignment = NSTextAlignment.justified
+        let attributes: Dictionary = [NSAttributedString.Key.paragraphStyle: textStyle, NSAttributedString.Key.font: textFont]
+        let attibute = NSAttributedString(string:text, attributes: attributes)
+        self.contentLabel.attributedText = attibute
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("error")
+    }
+
+}
+
+
 class QiitaViewModel {
     static func fetchArticle(completion: @escaping ([QiitaStruct]) -> Swift.Void) {
 
@@ -52,6 +132,7 @@ class QiitaViewController: UIViewController {
         tableView.dataSource = self
         tableView.delegate = self
         tableView.frame = view.frame
+        tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: "customCell")
         view.addSubview(tableView)
         QiitaViewModel.fetchArticle(completion: { (articles) in
             self.articles = articles
@@ -70,7 +151,7 @@ extension QiitaViewController: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
-        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! CustomTableViewCell
         let article = articles[indexPath.row]
         cell.textLabel?.text = article.title
         cell.detailTextLabel?.text = article.user.name
